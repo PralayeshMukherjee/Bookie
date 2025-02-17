cda677d460551b42630f43fcdc0af0faa5661d54
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-17 19:17:46 +0530

    change the mapping

--- Backend/src/main/java/com/raj/library/repository/BookRepo.java
+++ Backend/src/main/java/com/raj/library/repository/BookRepo.java
@@ -1,6 +1,7 @@
 package com.raj.library.repository;
 
+import com.raj.library.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepo extends JpaRepository<Long,String> {
+public interface BookRepo extends JpaRepository<Book,Long> {
 }
