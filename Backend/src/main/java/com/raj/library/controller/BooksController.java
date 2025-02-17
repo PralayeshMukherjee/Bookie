0d59884bbbd79f8ed37a7ad6a55f502c4e9cdd0d
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-17 20:12:10 +0530

    complete the function that return a map which is act like a json object where key is isAdded and value is which is return from book service class

--- Backend/src/main/java/com/raj/library/controller/BooksController.java
+++ Backend/src/main/java/com/raj/library/controller/BooksController.java
@@ -1,9 +1,13 @@
 package com.raj.library.controller;
 
+import com.raj.library.DTO.BookAdd;
 import com.raj.library.Service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/books")
@@ -11,4 +15,13 @@ public class BooksController {
 
     @Autowired
     private BookService bookService;
+
+    public Map<String,Boolean> addNewBook(@RequestBody BookAdd bookAdd){
+        String title = bookAdd.getTitle();
+        int price = Integer.parseInt(bookAdd.getPrice());
+        String stocks = bookAdd.getStocks();
+        String author = bookAdd.getAuthor();
+        boolean isAdded =  bookService.addBooks(title,price,stocks,author);
+        return Map.of("isAdded", isAdded);
+    }
 }
