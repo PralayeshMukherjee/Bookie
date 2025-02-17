12a5a99004e0609ccef667c92b314ec98f42ca67
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-17 19:31:04 +0530

    add the getter setter method for all fields

--- Backend/src/main/java/com/raj/library/DTO/BookAdd.java
+++ Backend/src/main/java/com/raj/library/DTO/BookAdd.java
@@ -8,4 +8,36 @@ public class BookAdd {
 
     public BookAdd() {
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getStocks() {
+        return stocks;
+    }
+
+    public void setStocks(String stocks) {
+        this.stocks = stocks;
+    }
 }
