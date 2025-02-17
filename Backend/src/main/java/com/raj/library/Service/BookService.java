7ac996734c4d88b6c849e91b82c92d3a5e97b147
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-17 20:02:45 +0530

    set the book fields

--- Backend/src/main/java/com/raj/library/Service/BookService.java
+++ Backend/src/main/java/com/raj/library/Service/BookService.java
@@ -1,5 +1,6 @@
 package com.raj.library.Service;
 
+import com.raj.library.entity.Book;
 import com.raj.library.repository.BookRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,13 @@ public class BookService {
 
     @Autowired
     private BookRepo bookRepo;
+
+    public boolean addBooks(String title,int price,String stocks,String author){
+        Book book = new Book();
+        book.setAuthor(author);
+        book.setTitle(title);
+        book.setPrice(price);
+        book.setStocks(stocks);
+
+    }
 }
