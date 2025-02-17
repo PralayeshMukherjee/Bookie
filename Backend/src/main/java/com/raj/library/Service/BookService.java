087ac6cca8970c6e6652ea964df6b52b9018da87
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-17 19:42:08 +0530

    add the book repository and autowired it

--- Backend/src/main/java/com/raj/library/Service/BookService.java
+++ Backend/src/main/java/com/raj/library/Service/BookService.java
@@ -1,7 +1,12 @@
 package com.raj.library.Service;
 
+import com.raj.library.repository.BookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
+
+    @Autowired
+    private BookRepo bookRepo;
 }
