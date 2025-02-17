5475961b20854944f81f88b5732cb7ae5f08dead
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-17 19:47:13 +0530

    add the instance of book service class

--- Backend/src/main/java/com/raj/library/controller/BooksController.java
+++ Backend/src/main/java/com/raj/library/controller/BooksController.java
@@ -1,5 +1,7 @@
 package com.raj.library.controller;
 
+import com.raj.library.Service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/books")
 public class BooksController {
 
-
-
+    @Autowired
+    private BookService bookService;
 }
