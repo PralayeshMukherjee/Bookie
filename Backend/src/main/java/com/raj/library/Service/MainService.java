7b915bbe045e5d9593d9d6f0ba7c12fb1a85120e
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-18 16:37:16 +0530

    add the Java Mail Sender instance and autowired it

--- Backend/src/main/java/com/raj/library/Service/MainService.java
+++ Backend/src/main/java/com/raj/library/Service/MainService.java
@@ -1,7 +1,15 @@
 package com.raj.library.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MainService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+    public boolean sendMailToContact(String username,String email,String mobile, String message){
+
+    }
 }
