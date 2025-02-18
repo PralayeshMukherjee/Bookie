3c5e332792f6c922c2298bf911c91ba08100dca3
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-18 16:31:16 +0530

    add the getter and setter method for all fields

--- Backend/src/main/java/com/raj/library/DTO/ContactHolder.java
+++ Backend/src/main/java/com/raj/library/DTO/ContactHolder.java
@@ -8,4 +8,36 @@ public class ContactHolder {
 
     public ContactHolder() {
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
