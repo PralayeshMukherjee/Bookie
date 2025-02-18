6e16a11e470dfe0eb0d99a7737e64cf6384754e0
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-18 15:02:01 +0530

    write the getter and setter method for access id

--- Backend/src/main/java/com/raj/library/DTO/BookDelete.java
+++ Backend/src/main/java/com/raj/library/DTO/BookDelete.java
@@ -5,4 +5,12 @@ public class BookDelete {
 
     public BookDelete() {
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
