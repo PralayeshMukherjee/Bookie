dfa7aa5a3847a724253d25141ebe778cf7064c8b
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-18 16:53:49 +0530

    change the return type which is act like map of string and boolean which is act like json key and value

--- Backend/src/main/java/com/raj/library/controller/MailController.java
+++ Backend/src/main/java/com/raj/library/controller/MailController.java
@@ -7,18 +7,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/main")
 public class MailController {
 
     @Autowired
     private MailService mailService;
-    public void sendMail(@RequestBody ContactHolder contactHolder){
+    public Map<String, Boolean> sendMail(@RequestBody ContactHolder contactHolder){
         String username = contactHolder.getUsername();
         String email = contactHolder.getEmail();
         String mobile = contactHolder.getTel();
         String message = contactHolder.getMessage();
 
         boolean isContactMailSend = mailService.sendMailToContact(username,email,mobile,message);
+        return Map.of("isContactMailSend",isContactMailSend);
     }
 }
