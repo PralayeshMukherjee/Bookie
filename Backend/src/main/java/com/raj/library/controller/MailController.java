3956021299af448e27ab53d35169b94e54b38364
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-18 16:46:55 +0530

    change the DTO method

--- Backend/src/main/java/com/raj/library/controller/MailController.java
+++ Backend/src/main/java/com/raj/library/controller/MailController.java
@@ -1,5 +1,6 @@
 package com.raj.library.controller;
 
+import com.raj.library.DTO.ContactHolder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class MailController {
 
-    public void sendMail(@RequestBody MailController mailController){
+    public void sendMail(@RequestBody ContactHolder contactHolder){
 
     }
 }
