3728198ecfc977b103da80e38de2ac3fd334a5bc
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-18 16:46:04 +0530

    add the method that send that call the service method

--- Backend/src/main/java/com/raj/library/controller/MailController.java
+++ Backend/src/main/java/com/raj/library/controller/MailController.java
@@ -1,5 +1,6 @@
 package com.raj.library.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class MailController {
 
+    public void sendMail(@RequestBody MailController mailController){
+
+    }
 }
