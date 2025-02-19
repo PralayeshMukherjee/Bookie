bac27d5949fb1049fd5eb725f61042b5235e007c
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-19 17:54:44 +0530

    add the getter and setter method for username

--- Backend/src/main/java/com/raj/library/DTO/OtpSender.java
+++ Backend/src/main/java/com/raj/library/DTO/OtpSender.java
@@ -13,4 +13,12 @@ public class OtpSender {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
