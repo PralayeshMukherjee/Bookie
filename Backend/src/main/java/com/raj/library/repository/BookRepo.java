415402d30f821ddc09eb623eb245b5f58c13284e
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-19 15:19:42 +0530

    add method that return the list of books from book table where the username is match

--- Backend/src/main/java/com/raj/library/repository/BookRepo.java
+++ Backend/src/main/java/com/raj/library/repository/BookRepo.java
@@ -3,5 +3,8 @@ package com.raj.library.repository;
 import com.raj.library.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepo extends JpaRepository<Book,Long> {
+    List<Book> findByUsername(String username);
 }
