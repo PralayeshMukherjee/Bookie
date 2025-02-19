5c7492eaa5e3cde5e346ba8ec08af1c521c49336
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-19 20:23:46 +0530

    add the mapping

--- Backend/src/main/java/com/raj/library/controller/VerificationController.java
+++ Backend/src/main/java/com/raj/library/controller/VerificationController.java
@@ -4,6 +4,7 @@ import com.raj.library.DTO.OTPVerification;
 import com.raj.library.Service.MailService;
 import com.raj.library.Service.OtpService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("verify")
+@RequestMapping("/verify")
 public class VerificationController {
 
     @Autowired
     private OtpService otpService;
 
+    @PostMapping("/otpVerification")
     public Map<String, Integer> verificationOTP(@RequestBody OTPVerification otpVerification){
         String username = otpVerification.getUsername();
         String otp = otpVerification.getOtp();
