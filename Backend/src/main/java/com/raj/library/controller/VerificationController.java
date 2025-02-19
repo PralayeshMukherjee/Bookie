a757904b446716b752de3a4ad408a86c0fa369bf
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-19 19:35:43 +0530

    add a method that call the otp service class methods to check the otp

--- Backend/src/main/java/com/raj/library/controller/VerificationController.java
+++ Backend/src/main/java/com/raj/library/controller/VerificationController.java
@@ -1,8 +1,10 @@
 package com.raj.library.controller;
 
+import com.raj.library.DTO.OTPVerification;
 import com.raj.library.Service.MailService;
 import com.raj.library.Service.OtpService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +14,8 @@ public class VerificationController {
 
     @Autowired
     private OtpService otpService;
+
+    public void verificationOTP(@RequestBody OTPVerification otpVerification){
+
+    }
 }
