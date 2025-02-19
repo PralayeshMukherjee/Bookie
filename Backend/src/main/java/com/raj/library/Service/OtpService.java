12c384cbdf720ef0180fca4a1cd488ef39140284
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-19 17:53:07 +0530

    change the return type of the generate OTP method from void to boolean and hold the otp data in map with username

--- Backend/src/main/java/com/raj/library/Service/OtpService.java
+++ Backend/src/main/java/com/raj/library/Service/OtpService.java
@@ -9,12 +9,12 @@ import java.util.Map;
 public class OtpService {
     private static final int otpLength = 6;
     private Map<String,String> otpMapping = new HashMap<>();
-    public void generateOTP(String username){
+    public boolean generateOTP(String username){
         String generatedOTP = "";
         for(int i=0;i<=otpLength;i++){
             generatedOTP += String.valueOf((Math.random()*9)+1);
         }
         otpMapping.put(username,generatedOTP);
+        return true;
     }
-
 }
