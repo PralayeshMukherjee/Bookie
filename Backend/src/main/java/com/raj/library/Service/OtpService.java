9bd283a4c39f8c89e8dccb63875501d6b8a42484
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-19 17:50:55 +0530

    add a method that generate OTP

--- Backend/src/main/java/com/raj/library/Service/OtpService.java
+++ Backend/src/main/java/com/raj/library/Service/OtpService.java
@@ -2,9 +2,19 @@ package com.raj.library.Service;
 
 import org.springframework.stereotype.Service;
 
-import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class OtpService {
-    private static final int optLength = 6;
+    private static final int otpLength = 6;
+    private Map<String,String> otpMapping = new HashMap<>();
+    public void generateOTP(String username){
+        String generatedOTP = "";
+        for(int i=0;i<=otpLength;i++){
+            generatedOTP += String.valueOf((Math.random()*9)+1);
+        }
+        otpMapping.put(username,generatedOTP);
+    }
+
 }
