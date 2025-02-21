40f6f3330ba69ae85e0736a268388f048a481769
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-22 02:27:59 +0530

    add a method that fetch title from database

--- Backend/src/main/java/com/raj/library/repository/SearchingRepo.java
+++ Backend/src/main/java/com/raj/library/repository/SearchingRepo.java
@@ -4,6 +4,9 @@ import com.raj.library.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SearchingRepo extends JpaRepository<Book,Long> {
+    List<Book> findByTitleContainingIgnoreCase(String title);
 }
