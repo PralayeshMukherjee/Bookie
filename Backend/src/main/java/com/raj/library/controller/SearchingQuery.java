a4212968f140f077e191b9e488e54a32bd542e19
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-22 02:36:05 +0530

    add a method that return list of String where title matches

--- Backend/src/main/java/com/raj/library/controller/SearchingQuery.java
+++ Backend/src/main/java/com/raj/library/controller/SearchingQuery.java
@@ -2,7 +2,10 @@ package com.raj.library.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/search")
@@ -10,4 +13,8 @@ public class SearchingQuery {
 
     @Autowired
     private SearchingQuery searchingQuery;
+
+    public List<String> searchBook(@RequestParam String title){
+
+    }
 }
