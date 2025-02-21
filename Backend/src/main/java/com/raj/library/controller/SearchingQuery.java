fa0d27a9342b18a6496c36535264a6d6b7a7fd51
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-22 02:36:53 +0530

    add the get mapping

--- Backend/src/main/java/com/raj/library/controller/SearchingQuery.java
+++ Backend/src/main/java/com/raj/library/controller/SearchingQuery.java
@@ -1,6 +1,7 @@
 package com.raj.library.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class SearchingQuery {
     @Autowired
     private SearchingQuery searchingQuery;
 
+    @GetMapping("/books")
     public List<String> searchBook(@RequestParam String query){
         return  searchingQuery.searchBook(query);
     }
