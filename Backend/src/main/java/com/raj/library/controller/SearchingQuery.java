3a89ae15c2490d1bd4b7c81b286d3824fc194a8e
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-22 02:32:20 +0530

    add the instance of Searching query and autowired it

--- Backend/src/main/java/com/raj/library/controller/SearchingQuery.java
+++ Backend/src/main/java/com/raj/library/controller/SearchingQuery.java
@@ -1,9 +1,13 @@
 package com.raj.library.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/search")
 public class SearchingQuery {
+
+    @Autowired
+    private SearchingQuery searchingQuery;
 }
