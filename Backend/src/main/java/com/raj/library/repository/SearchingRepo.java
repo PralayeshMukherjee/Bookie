0ceda5b998f229d7eeb2c7be3549e93e9f619c36
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-22 02:26:56 +0530

    add the repository annotation to denote this is a repository

--- Backend/src/main/java/com/raj/library/repository/SearchingRepo.java
+++ Backend/src/main/java/com/raj/library/repository/SearchingRepo.java
@@ -1,4 +1,9 @@
 package com.raj.library.repository;
 
-public interface SearchingRepo extai{
+import com.raj.library.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SearchingRepo extends JpaRepository<Book,Long> {
 }
