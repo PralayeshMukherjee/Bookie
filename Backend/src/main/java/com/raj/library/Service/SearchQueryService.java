ecd4d5952324afbf800f91260c6cdefd1fab2eaf
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-22 02:29:50 +0530

    add a method that return the list of Book entity

--- Backend/src/main/java/com/raj/library/Service/SearchQueryService.java
+++ Backend/src/main/java/com/raj/library/Service/SearchQueryService.java
@@ -1,12 +1,19 @@
 package com.raj.library.Service;
 
+import com.raj.library.entity.Book;
 import com.raj.library.repository.SellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SearchQueryService {
 
     @Autowired
     private SellerRepository sellerRepository;
+
+    public List<Book> getALlBooksWhereTitleMatched(String title){
+
+    }
 }
