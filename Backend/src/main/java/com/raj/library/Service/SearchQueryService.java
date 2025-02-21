8040e1f8464bb7b4da93869a130910d31ad9b604
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-22 02:28:37 +0530

    add the instance of Seller Repository and autowired it

--- Backend/src/main/java/com/raj/library/Service/SearchQueryService.java
+++ Backend/src/main/java/com/raj/library/Service/SearchQueryService.java
@@ -1,7 +1,12 @@
 package com.raj.library.Service;
 
+import com.raj.library.repository.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SearchQueryService {
+
+    @Autowired
+    private SellerRepository sellerRepository;
 }
