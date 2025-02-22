0ca6fa175cecd9a2a70483f351b3ed7c1e45c9b0
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-23 00:26:29 +0530

    and autowired the instance

--- Backend/src/main/java/com/raj/library/controller/BookSellers.java
+++ Backend/src/main/java/com/raj/library/controller/BookSellers.java
@@ -1,6 +1,7 @@
 package com.raj.library.controller;
 
 import com.raj.library.Service.BookSellersService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/fetchSellers")
 public class BookSellers {
 
+    @Autowired
     private BookSellersService bookSellersService;
 }
