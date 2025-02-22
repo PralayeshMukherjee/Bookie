4841334ab063bf16cf1a21f41571beb23150e18a
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-22 23:52:19 +0530

    add a async function

--- frontend/src/Component/ProductSection/Product.jsx
+++ frontend/src/Component/ProductSection/Product.jsx
@@ -2,7 +2,10 @@ import React, { useEffect } from "react";
 
 function Product() {
   const selectedBook = sessionStorage.getItem("selectedBook");
-  useEffect(() => {}, [selectedBook]);
+  useEffect(() => {
+    console.log(fetching);
+  }, [selectedBook]);
+  const fetchData = async () => {};
   return (
     <div>
       <h1>book found</h1>
