6d90f4ba455403b8e75cb25e291741a86c1524fd
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-23 00:19:11 +0530

    add a method that return the list of book where title matched

--- Backend/src/main/java/com/raj/library/Service/BookSellersService.java
+++ Backend/src/main/java/com/raj/library/Service/BookSellersService.java
@@ -1,12 +1,19 @@
 package com.raj.library.Service;
 
+import com.raj.library.entity.Book;
 import com.raj.library.repository.BookRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookSellersService {
 
     @Autowired
     private BookRepo bookRepo;
+
+    public List<Book> getAllBooksMatchedWithTitle(String title){
+        
+    }
 }
