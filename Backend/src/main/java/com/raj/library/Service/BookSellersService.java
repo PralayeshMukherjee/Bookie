0122a8569dbd54b4a1306e91de76ea5577c38678
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-23 00:15:38 +0530

    autowired the instance

--- Backend/src/main/java/com/raj/library/Service/BookSellersService.java
+++ Backend/src/main/java/com/raj/library/Service/BookSellersService.java
@@ -1,10 +1,12 @@
 package com.raj.library.Service;
 
 import com.raj.library.repository.BookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookSellersService {
 
+    @Autowired
     private BookRepo bookRepo;
 }
