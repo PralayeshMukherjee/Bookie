051352b34ea8b596d825c1465084492cf7a60b09
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-22 23:43:52 +0530

    remove onMouse method

--- frontend/src/Component/MainPages/MainHeader.jsx
+++ frontend/src/Component/MainPages/MainHeader.jsx
@@ -178,10 +178,6 @@ function MainHeader() {
                   <li
                     key={i}
                     className="px-4 py-2 hover:bg-gray-100 cursor-pointer text-black"
-                    onMouseDown={() => {
-                      setQuery(value);
-                      setShowDropdown(false);
-                    }}
                     onClick={() => ProductFound(value)}
                   >
                     {value}
