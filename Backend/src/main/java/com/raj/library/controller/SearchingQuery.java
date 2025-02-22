2a92a7e9dc4592a234564eeb9019df7e8ca6dfaf
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-22 19:43:11 +0530

    change the instance of service class

--- Backend/src/main/java/com/raj/library/controller/SearchingQuery.java
+++ Backend/src/main/java/com/raj/library/controller/SearchingQuery.java
@@ -1,5 +1,6 @@
 package com.raj.library.controller;
 
+import com.raj.library.Service.SearchQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +14,10 @@ import java.util.List;
 public class SearchingQuery {
 
     @Autowired
-    private SearchingQuery searchingQuery;
+    private SearchQueryService searchQueryService;
 
     @GetMapping("/books")
     public List<String> searchBook(@RequestParam String query){
-        return  searchingQuery.searchBook(query);
+        return  searchQueryService.getALlBooksWhereTitleMatched(query);
     }
 }
