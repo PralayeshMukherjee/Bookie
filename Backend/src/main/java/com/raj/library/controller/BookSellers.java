39e4a09683d7fffd584ff23992dd4953e625d03d
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-23 00:24:35 +0530

    add the instance of BookSellersService to access the method

--- Backend/src/main/java/com/raj/library/controller/BookSellers.java
+++ Backend/src/main/java/com/raj/library/controller/BookSellers.java
@@ -1,9 +1,12 @@
 package com.raj.library.controller;
 
+import com.raj.library.Service.BookSellersService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/fetchSellers")
 public class BookSellers {
+
+    private BookSellersService bookSellersService;
 }
