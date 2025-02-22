6861220aaa0cfec9a87eb7f2043ba172ddb4c625
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-23 00:12:27 +0530

    add the instance of BookRepo which extends JPA repository

--- Backend/src/main/java/com/raj/library/Service/BookSellersService.java
+++ Backend/src/main/java/com/raj/library/Service/BookSellersService.java
@@ -1,7 +1,10 @@
 package com.raj.library.Service;
 
+import com.raj.library.repository.BookRepo;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookSellersService {
+
+    private BookRepo bookRepo;
 }
