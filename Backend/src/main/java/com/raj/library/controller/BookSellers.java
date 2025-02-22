6f0bb71265c303b148aeac60b686aff5a43eb783
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-23 00:28:58 +0530

    add the method that return list of book and take title as parameter from RequestParam

--- Backend/src/main/java/com/raj/library/controller/BookSellers.java
+++ Backend/src/main/java/com/raj/library/controller/BookSellers.java
@@ -1,9 +1,13 @@
 package com.raj.library.controller;
 
 import com.raj.library.Service.BookSellersService;
+import com.raj.library.entity.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/fetchSellers")
@@ -11,4 +15,8 @@ public class BookSellers {
 
     @Autowired
     private BookSellersService bookSellersService;
+
+    public List<Book> getAllBook(@RequestParam String title){
+
+    }
 }
