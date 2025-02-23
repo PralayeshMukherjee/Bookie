e9ffeb77675faa0a8a2c6a97932d8307e133282e
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-23 23:42:44 +0530

    import the component

--- frontend/src/Component/index.js
+++ frontend/src/Component/index.js
@@ -27,5 +27,6 @@ import MailSuccess from '../Component/Success/MailSeccess.jsx'
 import Product from '../Component/ProductSection/Product.jsx'
 import Search from "../images/Search.png";
 import LoginPage from '../images/LoginPage.png'
+import ProductDetails from '../Component/ProductSection/ProductDetails.jsx';
 
-export {Layout,Home,UserLogin,UserRegister,UserRegisterSuccess,MainHeader,MainLayout,MainFooter,MainHome,About,Library,User,Profile,Setting,Help,Logout,Pic,userLoginPNG,AboutPNG,Clear,Search,SellerLogin,SellerRegister,SellerRegisterSuccess,SellerFeature,Contact,MailSuccess,Product,LoginPage};
+export {Layout,Home,UserLogin,UserRegister,UserRegisterSuccess,MainHeader,MainLayout,MainFooter,MainHome,About,Library,User,Profile,Setting,Help,Logout,Pic,userLoginPNG,AboutPNG,Clear,Search,SellerLogin,SellerRegister,SellerRegisterSuccess,SellerFeature,Contact,MailSuccess,Product,LoginPage,ProductDetails};
