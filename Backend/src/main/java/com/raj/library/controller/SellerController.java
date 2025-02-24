0875ddd2656c8e45a2938231597a4fd604f968bd
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-24 16:22:56 +0530

    add a method that return Map of string and boolean

--- Backend/src/main/java/com/raj/library/controller/SellerController.java
+++ Backend/src/main/java/com/raj/library/controller/SellerController.java
@@ -1,9 +1,17 @@
 package com.raj.library.controller;
 
+import com.raj.library.DTO.SellerContact;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/mailSenderSeller")
 public class SellerController {
+
+    public Map<String,Boolean> sendMailToSeller(@RequestBody SellerContact sellerContact){
+
+    }
 }
