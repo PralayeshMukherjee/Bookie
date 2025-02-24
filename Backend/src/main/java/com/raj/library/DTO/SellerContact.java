dbf6c99631b75b2f473268f8348d2b4e6ab6d606
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-24 16:18:24 +0530

    add the fields

--- Backend/src/main/java/com/raj/library/DTO/SellerContact.java
+++ Backend/src/main/java/com/raj/library/DTO/SellerContact.java
@@ -1,4 +1,8 @@
 package com.raj.library.DTO;
 
 public class SellerContact {
+    private String username;
+    private String email;
+    private String message;
+    private String sellerEmail;
 }
