067b894ea7f62bec2764c3dcefb994fad1073364
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-24 16:02:29 +0530

    update the code and send the request

--- frontend/src/Component/ProductSection/ContactSeller.jsx
+++ frontend/src/Component/ProductSection/ContactSeller.jsx
@@ -1,6 +1,109 @@
 import { FaEnvelope, FaPhone, FaMapMarkerAlt, FaClock } from "react-icons/fa";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const ContactSeller = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    message: "",
+  });
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const handleSendOtp = async (e) => {
+    e.preventDefault();
+    if (formData.email == "") {
+      alert("Please fill the email field first");
+    } else if (formData.username == "") {
+      alert("Please fill the username field first");
+    } else {
+      try {
+        const sendOTPResponse = await fetch(
+          "http://localhost:8080/mail/sendOtp",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              email: formData.email,
+              username: formData.username,
+            }),
+          }
+        );
+        const data = await sendOTPResponse.json();
+        console.log(data);
+        if (data.isMailSend) {
+          alert("OTP send to your email");
+        } else {
+          alert("OTP not send!!! Please try again later");
+        }
+      } catch (error) {
+        console.log("Error:- ", error);
+      }
+    }
+  };
+  const formSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    if (sessionStorage.getItem("isVerified") != "true") {
+      alert("Please verify the OTP first");
+      return;
+    }
+    try {
+      const response = await fetch("http://localhost:8080/mail/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const result = await response.json();
+      console.log(result);
+      if (result.isContactMailSend) {
+        sessionStorage.setItem("isContactMailSend", "true");
+        setFormData({});
+        navigate("/MailSuccess");
+      } else {
+        alert(
+          "Message not send!!! Please fill the form correcty or try again later"
+        );
+      }
+    } catch (error) {
+      console.log("Error:- ", error);
+    }
+  };
+  const [otp, setOtp] = useState({ otp: "" });
+  const otpChange = (e) => {
+    setOtp({ otp: e.target.value });
+  };
+  const verifyOTP = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:8080/verify/verifyOTP", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ otp: otp.otp, username: formData.username }),
+      });
+      const result = await response.json();
+      if (result.isVerified == 2) {
+        alert("OTP Verified");
+        sessionStorage.setItem("isVerified", "true");
+      } else if (result.isVerified == 1) {
+        alert("OTP is incorrect");
+      } else {
+        alert("User not found");
+      }
+    } catch (error) {
+      console.log("Error:- ", error);
+    }
+  };
+
   return (
     <div className="flex flex-col md:flex-row min-h-screen bg-gradient-to-r from-violet-200 to-violet-400 text-black p-6 md:p-16">
       {/* Left Section */}
@@ -12,18 +115,45 @@ const ContactSeller = () => {
           Feel free to contact us any time. We will get back to you as soon as
           we can!
         </p>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={formSubmit}>
           <input
             type="text"
-            placeholder="Name"
+            name="username"
+            onChange={handleChange}
+            placeholder="Username"
             className="w-full p-4 border border-violet-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-500"
           />
           <input
+            name="email"
+            onChange={handleChange}
             type="email"
             placeholder="Email"
             className="w-full p-4 border border-violet-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-500"
           />
+          <button
+            onClick={handleSendOtp}
+            className="bg-violet-400 w-20 h-7 mt-1 rounded-lg font-semibold text-white hover:bg-violet-600 transition ease-in-out duration-300"
+          >
+            Send OTP
+          </button>
+          <input
+            type="number"
+            name="otp"
+            id="otp"
+            placeholder="OTP via Email"
+            className="w-100 mt-2 py-3 px-3 rounded-lg bg-violet-200 border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:border-2 focus:outline-none"
+            onChange={otpChange}
+            required
+          />
+          <button
+            onClick={verifyOTP}
+            className="bg-violet-400 w-20 h-7 mt-1 rounded-lg font-semibold text-white hover:bg-violet-600 transition ease-in-out duration-300"
+          >
+            Verify
+          </button>
           <textarea
+            name="message"
+            onChange={handleChange}
             placeholder="Message"
             className="w-full p-4 border border-violet-300 rounded-lg h-32 focus:outline-none focus:ring-2 focus:ring-violet-500"
           ></textarea>
