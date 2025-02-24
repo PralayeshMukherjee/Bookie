51bc06b17dcea4d17c684ecef043b1dfa6a634d0
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-24 13:22:32 +0530

    add a method that return the name of the seller who sell the particular book

--- Backend/src/main/java/com/raj/library/controller/BookSellers.java
+++ Backend/src/main/java/com/raj/library/controller/BookSellers.java
@@ -27,4 +27,7 @@ public class BookSellers {
         System.out.println(bookId);
         return bookSellersService.getBookWhereIdMatched(bookId);
     }
+    public String SellerName(){
+
+    }
 }
