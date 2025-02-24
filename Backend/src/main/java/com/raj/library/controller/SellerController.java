1ba4df95e2e3c17e5789bf258e7a7aa847205662
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-24 16:32:07 +0530

    add the instance of mail service and autowired it

--- Backend/src/main/java/com/raj/library/controller/SellerController.java
+++ Backend/src/main/java/com/raj/library/controller/SellerController.java
@@ -1,6 +1,8 @@
 package com.raj.library.controller;
 
 import com.raj.library.DTO.SellerContact;
+import com.raj.library.Service.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,9 @@ import java.util.Map;
 @RestController
 @RequestMapping("/mailSenderSeller")
 public class SellerController {
+
+    @Autowired
+    private MailService mailService;
 
     @PostMapping("/sendMailSeller")
     public Map<String,Boolean> sendMailToSeller(@RequestBody SellerContact sellerContact){
