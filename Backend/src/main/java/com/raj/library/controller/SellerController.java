76f83dbfb06f6980b765dc87e6c03978ac6d5a7b
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-24 16:24:38 +0530

    add the post mapping

--- Backend/src/main/java/com/raj/library/controller/SellerController.java
+++ Backend/src/main/java/com/raj/library/controller/SellerController.java
@@ -1,6 +1,7 @@
 package com.raj.library.controller;
 
 import com.raj.library.DTO.SellerContact;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.Map;
 @RequestMapping("/mailSenderSeller")
 public class SellerController {
 
+    @PostMapping("/sendMailSeller")
     public Map<String,Boolean> sendMailToSeller(@RequestBody SellerContact sellerContact){
 
     }
