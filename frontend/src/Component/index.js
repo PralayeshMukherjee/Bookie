40fe2539a10177d90c92b89c8d1b317852d504cf
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-25 23:26:44 +0530

    import the DeliveryAddress component

--- frontend/src/Component/index.js
+++ frontend/src/Component/index.js
@@ -30,5 +30,6 @@ import LoginPage from '../images/LoginPage.png'
 import ProductDetails from '../Component/ProductSection/ProductDetails.jsx';
 import sellerContact from '../images/sellerContact.svg'
 import ContactSeller from '../Component/ProductSection/ContactSeller.jsx'
+import DeliveryAddress from '../Component/ProductSection/DeliveryAddress.jsx'
 
-export {Layout,Home,UserLogin,UserRegister,UserRegisterSuccess,MainHeader,MainLayout,MainFooter,MainHome,About,Library,User,Profile,Setting,Help,Logout,Pic,userLoginPNG,AboutPNG,Clear,Search,SellerLogin,SellerRegister,SellerRegisterSuccess,SellerFeature,Contact,MailSuccess,Product,LoginPage,ProductDetails,sellerContact,ContactSeller};
+export {Layout,Home,UserLogin,UserRegister,UserRegisterSuccess,MainHeader,MainLayout,MainFooter,MainHome,About,Library,User,Profile,Setting,Help,Logout,Pic,userLoginPNG,AboutPNG,Clear,Search,SellerLogin,SellerRegister,SellerRegisterSuccess,SellerFeature,Contact,MailSuccess,Product,LoginPage,ProductDetails,sellerContact,ContactSeller,DeliveryAddress};
