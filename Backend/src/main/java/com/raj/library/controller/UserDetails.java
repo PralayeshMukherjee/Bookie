45e5a1d271f60070e902ea8305f3b6300f93db17
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-26 01:10:32 +0530

    add a method that return type is String and hold the username data get from clint in param

--- Backend/src/main/java/com/raj/library/controller/UserDetails.java
+++ Backend/src/main/java/com/raj/library/controller/UserDetails.java
@@ -3,6 +3,7 @@ package com.raj.library.controller;
 import com.raj.library.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +12,8 @@ public class UserDetails {
 
     @Autowired
     private UserService userService;
+
+    public String getUserIdByUsername(@RequestParam String username){
+
+    }
 }
