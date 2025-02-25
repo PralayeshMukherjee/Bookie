2ade05072a2fd94e00863674c6c3eaa5532a734c
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-26 01:08:56 +0530

    add the instance of User Service and autowired it

--- Backend/src/main/java/com/raj/library/controller/UserDetails.java
+++ Backend/src/main/java/com/raj/library/controller/UserDetails.java
@@ -1,9 +1,14 @@
 package com.raj.library.controller;
 
+import com.raj.library.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/UserDetails")
 public class UserDetails {
+
+    @Autowired
+    private UserService userService;
 }
