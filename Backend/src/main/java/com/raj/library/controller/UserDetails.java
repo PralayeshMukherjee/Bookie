6d5dc1d450dc800c859502a4e5793c16592a4aa3
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-26 01:25:19 +0530

    add a method that return map of string and boolean and hold the data come from clintside in a Custom layer

--- Backend/src/main/java/com/raj/library/controller/UserDetails.java
+++ Backend/src/main/java/com/raj/library/controller/UserDetails.java
@@ -1,10 +1,14 @@
 package com.raj.library.controller;
 
+import com.raj.library.DTO.UserAddress;
 import com.raj.library.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/UserDetails")
@@ -15,5 +19,8 @@ public class UserDetails {
 
     public Long getUserIdByUsername(@RequestParam String username){
         return userService.getUserIdByUsernameService(username);
+    }
+    public Map<String,Boolean> saveUserAddress(@RequestBody UserAddress userAddress){
+
     }
 }
