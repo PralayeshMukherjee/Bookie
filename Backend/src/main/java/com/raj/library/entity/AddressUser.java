d801d76f050b7bb01e99bd264e2ec877a6015d58
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-26 01:39:40 +0530

    add the Id annotation on pincode to make it primary key

--- Backend/src/main/java/com/raj/library/entity/AddressUser.java
+++ Backend/src/main/java/com/raj/library/entity/AddressUser.java
@@ -1,9 +1,23 @@
 package com.raj.library.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="UserAddress")
 public class AddressUser {
+    @Id
+    private String pincode;
+    private String name;
+    private String phone;
+    private String locality;
+    private String address;
+    private String city;
+    private String state;
+    private String landmark;
+    private String alternatePhone;
+    private String addressType;
+    private String username;
+    private String id;
 }
