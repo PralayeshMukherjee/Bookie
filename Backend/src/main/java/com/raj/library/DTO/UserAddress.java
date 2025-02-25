3b2ef77c5f89722f3381fa1b6a081a707d24431d
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-26 01:17:13 +0530

    add all fields of User address

--- Backend/src/main/java/com/raj/library/DTO/UserAddress.java
+++ Backend/src/main/java/com/raj/library/DTO/UserAddress.java
@@ -1,4 +1,16 @@
 package com.raj.library.DTO;
 
 public class UserAddress {
+    private String name;
+    private String phone;
+    private String pincode;
+    private String locality;
+    private String address;
+    private String city;
+    private String state;
+    private String landmark;
+    private String alternatePhone;
+    private String addressType;
+    private String username;
+    private String id;
 }
