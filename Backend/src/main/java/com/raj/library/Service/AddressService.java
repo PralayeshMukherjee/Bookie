a2975bed964cfde200e0bcfd80a8f94157d2798b
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-26 16:59:28 +0530

    add the instance of UserAddressRepo and autowired it

--- Backend/src/main/java/com/raj/library/Service/AddressService.java
+++ Backend/src/main/java/com/raj/library/Service/AddressService.java
@@ -1,7 +1,12 @@
 package com.raj.library.Service;
 
+import com.raj.library.repository.UserAddressRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddressService {
+
+    @Autowired
+    private UserAddressRepo userAddressRepo;
 }
