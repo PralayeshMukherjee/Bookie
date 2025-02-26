ae1d82daa3ec79f5e917331c0e9197f29e7c61fc
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-26 17:13:19 +0530

    set the fields

--- Backend/src/main/java/com/raj/library/Service/AddressService.java
+++ Backend/src/main/java/com/raj/library/Service/AddressService.java
@@ -1,5 +1,6 @@
 package com.raj.library.Service;
 
+import com.raj.library.entity.AddressUser;
 import com.raj.library.repository.UserAddressRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,19 @@ public class AddressService {
     private UserAddressRepo userAddressRepo;
 
     public boolean AddAddressOfUserService(String name,String phone,String pincode,String locality,String address,String city,String state,String landmark,String alternatePhone,String addressType,String username,Long id){
+        AddressUser addressUser = new AddressUser();
+        addressUser.setName(name);
+        addressUser.setPhone(phone);
+        addressUser.setPincode(pincode);
+        addressUser.setLocality(locality);
+        addressUser.setAddress(address);
+        addressUser.setCity(city);
+        addressUser.setState(state);
+        addressUser.setLandmark(landmark);
+        addressUser.setAlternatePhone(alternatePhone);
+        addressUser.setAddressType(addressType);
+        addressUser.setUsername(username);
+        addressUser.setId(id);
 
     }
 }
