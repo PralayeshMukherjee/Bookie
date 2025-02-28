e009e7fcedd90f798fde1536028d57c22d2afbf1
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-28 23:18:10 +0530

    use a method that return AddressUser entity from database using JPA repository

--- Backend/src/main/java/com/raj/library/repository/UserAddressRepo.java
+++ Backend/src/main/java/com/raj/library/repository/UserAddressRepo.java
@@ -4,4 +4,5 @@ import com.raj.library.entity.AddressUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserAddressRepo extends JpaRepository<AddressUser,Long> {
+    AddressUser findByUsername(String username);
 }
