7113b51f8739737103b761130b0b828d734abe16
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-02 00:45:09 +0530

    add some changes

--- frontend/src/Component/ProductSection/PaymentGetWay.jsx
+++ frontend/src/Component/ProductSection/PaymentGetWay.jsx
@@ -3,6 +3,15 @@ import { RadioGroup } from "@headlessui/react";
 import { CheckCircleIcon } from "@heroicons/react/24/solid";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+
+import gpay from "../../images/gpay.png";
+import phonepe from "../../images/phonepe.png";
+import paytmwallet from "../../images/paytmwallet.png";
+import amazonpay from "../../images/amazonpay.png";
+import mobikwik from "../../images/mobikwik.svg";
+import freecharge from "../../images/freecharge.png";
 
 const paymentMethods = [
   {
@@ -10,19 +19,14 @@ const paymentMethods = [
     name: "UPI",
     description: "Pay by any UPI app",
     options: ["Google Pay", "PhonePe", "Paytm", "Amazon Pay"],
-    images: ["/gpay.png", "/phonepe.png", "/paytm.png", "/amazonpay.png"],
+    images: [gpay, phonepe, paytmwallet, amazonpay],
   },
   {
     id: "wallets",
     name: "Wallets",
     description: "Select your wallet",
     options: ["Paytm Wallet", "Mobikwik", "FreeCharge", "Amazon Pay"],
-    images: [
-      "/paytmwallet.png",
-      "/mobikwik.png",
-      "/freecharge.png",
-      "/amazonpay.png",
-    ],
+    images: [paytmwallet, mobikwik, freecharge, amazonpay],
   },
   {
     id: "card",
@@ -56,7 +60,7 @@ export default function PaymentGetWay() {
         <RadioGroup value={selectedMethod} onChange={setSelectedMethod}>
           <div className="space-y-4">
             {paymentMethods.map((method) => (
-              <RadioGroup.Option key={method.id} value={method}>
+              <RadioGroup.Radio key={method.id} value={method} as="div">
                 {({ checked }) => (
                   <div
                     className={`p-4 border rounded-lg cursor-pointer ${
@@ -78,9 +82,9 @@ export default function PaymentGetWay() {
                     </div>
                     {checked && method.options.length > 0 && (
                       <div className="mt-2 pl-4 space-y-1">
-                        {method.options.map((option) => (
+                        {method.options.map((option, index) => (
                           <div
-                            key={option}
+                            key={index}
                             className="flex items-center space-x-2"
                           >
                             <input
@@ -95,16 +99,22 @@ export default function PaymentGetWay() {
                     )}
                     {checked && method.images && (
                       <Swiper
+                        modules={[Pagination]}
+                        pagination={{ clickable: true }}
                         spaceBetween={10}
                         slidesPerView={3}
+                        loop={true}
                         className="mt-2"
                       >
                         {method.images.map((image, index) => (
-                          <SwiperSlide key={index}>
+                          <SwiperSlide
+                            key={index}
+                            className="flex justify-center"
+                          >
                             <img
                               src={image}
                               alt={method.name}
-                              className="w-12 h-12"
+                              className="w-16 h-16 object-contain"
                             />
                           </SwiperSlide>
                         ))}
@@ -112,12 +122,11 @@ export default function PaymentGetWay() {
                     )}
                   </div>
                 )}
-              </RadioGroup.Option>
+              </RadioGroup.Radio>
             ))}
           </div>
         </RadioGroup>
       </div>
-
       <div className="md:w-1/3 mt-6 md:mt-0 md:ml-6 bg-white p-6 rounded-lg shadow-md">
         <h2 className="text-xl font-semibold mb-4">Price Details</h2>
         <div className="space-y-2 text-gray-700">
