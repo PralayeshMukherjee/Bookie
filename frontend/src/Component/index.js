8b500491c1c4c0c8b3dcfbb2c5dd632246c35a76
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-07 16:23:02 +0530

    import the functions

--- frontend/src/Component/index.js
+++ frontend/src/Component/index.js
@@ -33,8 +33,7 @@ import ContactSeller from '../Component/ProductSection/ContactSeller.jsx'
 import DeliveryAddress from '../Component/ProductSection/DeliveryAddress.jsx'
 import AddAddress from '../Component/ProductSection/AddAddress.jsx'
 import PaymentGetWay from '../Component/ProductSection/PaymentGetWay.jsx'
-import SettingSection from '../Component/Me/Setting.jsx'
-import EditSectionSeller from '../Component/Me/EditSectionSeller.jsx'
+import SettingSection from '../Component/Me/SettingSection.jsx'
+import EditSection from '../Component/Me/EditSection.jsx'
 
-
-export {Layout,Home,UserLogin,UserRegister,UserRegisterSuccess,MainHeader,MainLayout,MainFooter,MainHome,About,Library,User,Profile,Setting,Help,Logout,Pic,userLoginPNG,AboutPNG,Clear,Search,SellerLogin,SellerRegister,SellerRegisterSuccess,SellerFeature,Contact,MailSuccess,Product,LoginPage,ProductDetails,sellerContact,ContactSeller,DeliveryAddress,AddAddress,PaymentGetWay,SettingSection,EditSectionSeller};
+export {Layout,Home,UserLogin,UserRegister,UserRegisterSuccess,MainHeader,MainLayout,MainFooter,MainHome,About,Library,User,Profile,Setting,Help,Logout,Pic,userLoginPNG,AboutPNG,Clear,Search,SellerLogin,SellerRegister,SellerRegisterSuccess,SellerFeature,Contact,MailSuccess,Product,LoginPage,ProductDetails,sellerContact,ContactSeller,DeliveryAddress,AddAddress,PaymentGetWay,SettingSection,EditSection};
