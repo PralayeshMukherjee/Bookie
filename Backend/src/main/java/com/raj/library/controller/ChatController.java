86e76a17e9b5b1ff6ac797caca68b1defc55aeb5
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-15 00:15:53 +0530

    add a method that call the chat method in service class which interact with Gemini AI

--- Backend/src/main/java/com/raj/library/controller/ChatController.java
+++ Backend/src/main/java/com/raj/library/controller/ChatController.java
@@ -2,12 +2,17 @@ package com.raj.library.controller;
 
 import com.raj.library.Service.GeminiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api")
 public class ChatController {
     @Autowired
     private GeminiService geminiService;
+    public Mono<String> chat(@RequestBody String userMessage){
+        return geminiService.chat(userMessage);
+    }
 }
