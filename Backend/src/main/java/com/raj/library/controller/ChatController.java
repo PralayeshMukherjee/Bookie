76a61e81c0eaf42d1d9079a00a59b3e439165e82
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-15 00:10:22 +0530

    change the request mapping

--- Backend/src/main/java/com/raj/library/controller/ChatController.java
+++ Backend/src/main/java/com/raj/library/controller/ChatController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/chat")
+@RequestMapping("/api")
 public class ChatController {
     @Autowired
     private GeminiService geminiService;
