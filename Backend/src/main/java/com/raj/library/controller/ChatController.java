29449e4088f5f9b4de6eb65bcccec2142f5ba686
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-15 00:09:55 +0530

    add the instance of GeminiService and autowired it

--- Backend/src/main/java/com/raj/library/controller/ChatController.java
+++ Backend/src/main/java/com/raj/library/controller/ChatController.java
@@ -1,10 +1,13 @@
 package com.raj.library.controller;
 
+import com.raj.library.Service.GeminiService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/chat")
 public class ChatController {
-
+    @Autowired
+    private GeminiService geminiService;
 }
