26ab61457d44e71d56682bc6df4dcb007a06f8d8
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-15 00:16:21 +0530

    add a postmapping

--- Backend/src/main/java/com/raj/library/controller/ChatController.java
+++ Backend/src/main/java/com/raj/library/controller/ChatController.java
@@ -2,6 +2,7 @@ package com.raj.library.controller;
 
 import com.raj.library.Service.GeminiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import reactor.core.publisher.Mono;
 public class ChatController {
     @Autowired
     private GeminiService geminiService;
+    @PostMapping("/chat")
     public Mono<String> chat(@RequestBody String userMessage){
         return geminiService.chat(userMessage);
     }
