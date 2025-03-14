9e00a84c8841c7eb01533294df211d5c0fd1aa09
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-14 23:52:44 +0530

    add a value annotation for gemini api key

--- Backend/src/main/java/com/raj/library/Service/GeminiService.java
+++ Backend/src/main/java/com/raj/library/Service/GeminiService.java
@@ -1,8 +1,10 @@
 package com.raj.library.Service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GeminiService {
+    @Value("${gemini.api.key}")
     private String apiKey;
 }
