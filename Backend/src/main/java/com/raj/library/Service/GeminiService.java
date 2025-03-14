643d6bfd2073af5c52578102a353e69ee376491e
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-14 23:54:06 +0530

    add the instance of WebClint

--- Backend/src/main/java/com/raj/library/Service/GeminiService.java
+++ Backend/src/main/java/com/raj/library/Service/GeminiService.java
@@ -2,9 +2,13 @@ package com.raj.library.Service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class GeminiService {
     @Value("${gemini.api.key}")
     private String apiKey;
+
+    private final WebClient webClient;
+
 }
