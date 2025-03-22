6a792d0b52c6424e8e1ca3a31fb5fd2ca9a9bc76
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-22 20:48:17 +0530

    write the Repository annotation to make the file as Repository for library

--- Backend/src/main/java/com/raj/library/repository/LibraryRepo.java
+++ Backend/src/main/java/com/raj/library/repository/LibraryRepo.java
@@ -2,7 +2,9 @@ package com.raj.library.repository;
 
 import com.raj.library.entity.Library;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LibraryRepo extends JpaRepository<Library,Long> {
 
 }
