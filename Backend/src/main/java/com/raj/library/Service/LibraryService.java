ec1f92fa9fb9bff90aafe45220a3af492ce49db6
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-22 20:59:44 +0530

    add a try-catch block if library save in database than return true else throw an exception and return false

--- Backend/src/main/java/com/raj/library/Service/LibraryService.java
+++ Backend/src/main/java/com/raj/library/Service/LibraryService.java
@@ -1,5 +1,6 @@
 package com.raj.library.Service;
 
+import com.raj.library.entity.Library;
 import com.raj.library.repository.LibraryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,7 +9,14 @@ import org.springframework.stereotype.Service;
 public class LibraryService {
     @Autowired
     private LibraryRepo libraryRepo;
-    public boolean AddLibrary(String name,String password,String mailId,String ph){
-
+    public boolean AddLibrary(String username, String password, String mailId, String ph){
+        Library library = new Library(username,password,ph,mailId);
+        try{
+            libraryRepo.save(library);
+            return true;
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 }
