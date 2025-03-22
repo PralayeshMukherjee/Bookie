20bcc2413ede0720d71439560d5439da2c3d7814
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-22 21:52:58 +0530

    add the getter and setter method for private data members in DTO layer

--- Backend/src/main/java/com/raj/library/DTO/LibraryLoginDTO.java
+++ Backend/src/main/java/com/raj/library/DTO/LibraryLoginDTO.java
@@ -6,4 +6,20 @@ public class LibraryLoginDTO {
 
     public LibraryLoginDTO() {
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
