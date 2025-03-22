e0b2f4c1092e11a1ab417b54f12eacf5e8a29d93
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-22 20:50:21 +0530

    add the instance of libraryRepo instance and autowired it

--- Backend/src/main/java/com/raj/library/Service/LibraryService.java
+++ Backend/src/main/java/com/raj/library/Service/LibraryService.java
@@ -1,7 +1,11 @@
 package com.raj.library.Service;
 
+import com.raj.library.repository.LibraryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LibraryService {
+    @Autowired
+    private LibraryRepo libraryRepo;
 }
