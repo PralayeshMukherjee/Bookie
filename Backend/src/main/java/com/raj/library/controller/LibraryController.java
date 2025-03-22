84e42ca8bd61daf2eccd3dfc3a66eebf11db6a95
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-22 21:49:38 +0530

    add the postmapping on the method

--- Backend/src/main/java/com/raj/library/controller/LibraryController.java
+++ Backend/src/main/java/com/raj/library/controller/LibraryController.java
@@ -3,6 +3,7 @@ package com.raj.library.controller;
 import com.raj.library.DTO.LibraryRegDTO;
 import com.raj.library.Service.LibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.Map;
 public class LibraryController {
     @Autowired
     private LibraryService libraryService;
+    @PostMapping("/addLibrary")
     public Map<String,Boolean> AddLibraryController(@RequestBody LibraryRegDTO libraryRegDTO){
         String username = libraryRegDTO.getUsername();
         String password = libraryRegDTO.getPassword();
