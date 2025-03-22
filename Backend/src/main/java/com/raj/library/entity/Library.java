50aff38d9cefef8de30f90103bf2778203e3ec7b
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-22 20:42:48 +0530

    add a annotation to make id auto generated and unique

--- Backend/src/main/java/com/raj/library/entity/Library.java
+++ Backend/src/main/java/com/raj/library/entity/Library.java
@@ -1,13 +1,12 @@
 package com.raj.library.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="Library")
 public class Library {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String librarian;
