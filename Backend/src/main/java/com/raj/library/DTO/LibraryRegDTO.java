08b0c757565fe1d1a4afca175587362195f78ffb
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-22 21:32:04 +0530

    add the empty constructor to deserelization of data come from JSON

--- Backend/src/main/java/com/raj/library/DTO/LibraryRegDTO.java
+++ Backend/src/main/java/com/raj/library/DTO/LibraryRegDTO.java
@@ -8,4 +8,36 @@ public class LibraryRegDTO {
 
     public LibraryRegDTO() {
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPh() {
+        return ph;
+    }
+
+    public void setPh(String ph) {
+        this.ph = ph;
+    }
+
+    public String getMailId() {
+        return mailId;
+    }
+
+    public void setMailId(String mailId) {
+        this.mailId = mailId;
+    }
 }
