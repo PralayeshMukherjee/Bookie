d69c491889cb015899e28f7b454e8b0dea5e0473
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-22 20:41:42 +0530

    add other members

--- Backend/src/main/java/com/raj/library/entity/Library.java
+++ Backend/src/main/java/com/raj/library/entity/Library.java
@@ -1,10 +1,16 @@
 package com.raj.library.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="Library")
 public class Library {
     private Long id;
+    private String name;
+    private String librarian;
+    private String ph;
+    private String mailId;
+    private byte[] imageData;
 }
