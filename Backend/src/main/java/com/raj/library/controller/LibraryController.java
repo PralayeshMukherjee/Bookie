af0766de7171ff274385c837cd2d940ba8cc08fb
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-24 00:27:08 +0530

    autowired it for DI

--- Backend/src/main/java/com/raj/library/controller/LibraryController.java
+++ Backend/src/main/java/com/raj/library/controller/LibraryController.java
@@ -1,11 +1,13 @@
 package com.raj.library.controller;
 
 import com.raj.library.Service.LibraryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/library")
 public class LibraryController {
+    @Autowired
     private LibraryService libraryService;
 }
