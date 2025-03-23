4af3e81efe254863a29fe7634340b35361c55143
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-23 23:56:17 +0530

    add the library data members

--- Backend/src/main/java/com/raj/library/entity/Library.java
+++ Backend/src/main/java/com/raj/library/entity/Library.java
@@ -1,4 +1,16 @@
 package com.raj.library.entity;
 
 public class Library {
+    private String  name;
+    private float avgRating;
+    private String libraryMailId;
+    private double latitude;
+    private double longitude;
+    private String websiteLink;
+    private String openingTime;
+    private String closingTime;
+    private String openDays;
+    private String ph;
+    private String username;
+
 }
