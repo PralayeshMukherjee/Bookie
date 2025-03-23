e4514bf9c02162c5351c739e6137410cd2d3501b
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-24 00:28:36 +0530

    add a method that return Map of String and boolean

--- Backend/src/main/java/com/raj/library/controller/LibraryController.java
+++ Backend/src/main/java/com/raj/library/controller/LibraryController.java
@@ -1,13 +1,20 @@
 package com.raj.library.controller;
 
+import com.raj.library.DTO.LibraryAddingDTO;
 import com.raj.library.Service.LibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/library")
 public class LibraryController {
     @Autowired
     private LibraryService libraryService;
+    public Map<String,Boolean> addLibrary(@RequestBody LibraryAddingDTO libraryAddingDTO){
+
+    }
 }
