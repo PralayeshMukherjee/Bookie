253cfca11014d183bd43313b24e42dea214fa555
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-24 00:26:56 +0530

    add the instance of library service

--- Backend/src/main/java/com/raj/library/controller/LibraryController.java
+++ Backend/src/main/java/com/raj/library/controller/LibraryController.java
@@ -1,9 +1,11 @@
 package com.raj.library.controller;
 
+import com.raj.library.Service.LibraryService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/library")
 public class LibraryController {
+    private LibraryService libraryService;
 }
