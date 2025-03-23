b2ce93a1cd068928b7c1e96777773976fede970b
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-24 00:18:00 +0530

    add the instance of library repo

--- Backend/src/main/java/com/raj/library/Service/LibraryService.java
+++ Backend/src/main/java/com/raj/library/Service/LibraryService.java
@@ -1,7 +1,9 @@
 package com.raj.library.Service;
 
+import com.raj.library.repository.LibraryRepo;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LibraryService {
+    private LibraryRepo libraryRepo;
 }
