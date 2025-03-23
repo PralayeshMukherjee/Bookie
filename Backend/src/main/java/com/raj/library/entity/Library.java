02128c63dcc13d806e1334851d3f177f8a86361f
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-24 00:03:36 +0530

    add an parameterize constructor

--- Backend/src/main/java/com/raj/library/entity/Library.java
+++ Backend/src/main/java/com/raj/library/entity/Library.java
@@ -22,4 +22,18 @@ public class Library {
 
     public Library() {
     }
+
+    public Library(String name, float avgRating, String libraryMailId, double latitude, double longitude, String websiteLink, String openingTime, String closingTime, String openDays, String ph, String username) {
+        this.name = name;
+        this.avgRating = avgRating;
+        this.libraryMailId = libraryMailId;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.websiteLink = websiteLink;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+        this.openDays = openDays;
+        this.ph = ph;
+        this.username = username;
+    }
 }
