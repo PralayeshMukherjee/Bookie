a5b0bceb4188783ba1ef3a2ed65cecd236e6f72e
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-24 00:31:45 +0530

    craete the object library and call the parameterize constructor

--- Backend/src/main/java/com/raj/library/controller/LibraryController.java
+++ Backend/src/main/java/com/raj/library/controller/LibraryController.java
@@ -2,6 +2,7 @@ package com.raj.library.controller;
 
 import com.raj.library.DTO.LibraryAddingDTO;
 import com.raj.library.Service.LibraryService;
+import com.raj.library.entity.Library;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,7 @@ public class LibraryController {
     @Autowired
     private LibraryService libraryService;
     public Map<String,Boolean> addLibrary(@RequestBody LibraryAddingDTO libraryAddingDTO){
-        String  name = libraryAddingDTO.getName();
+        String name = libraryAddingDTO.getName();
         float avgRating = libraryAddingDTO.getAvgRating();
         String libraryMailId = libraryAddingDTO.getLibraryMailId();
         double latitude = libraryAddingDTO.getLatitude();
@@ -26,5 +27,6 @@ public class LibraryController {
         String openDays = libraryAddingDTO.getOpenDays();
         String ph = libraryAddingDTO.getLibraryMailId();
         String username = libraryAddingDTO.getUsername();
+        Library library = new Library(name,avgRating,libraryMailId,latitude,longitude,websiteLink,openingTime,closingTime,openDays,ph,username);
     }
 }
