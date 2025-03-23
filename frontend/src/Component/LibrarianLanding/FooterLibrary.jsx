07301551e474eb3cc8de9cf7f65d5ff77bb92fc1
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-23 20:55:43 +0530

    add a library footer page

---
+++ frontend/src/Component/LibrarianLanding/FooterLibrary.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+export default function FooterLibrary() {
+  <div className="min-h-screen bg-white font-sans flex flex-col justify-between">
+    <footer className="bg-gray-800 text-white text-center p-6 mt-12">
+      <p className="text-sm">&copy; 2025 Lando. All rights reserved.</p>
+      <ul className="flex justify-center space-x-6 mt-2">
+        <li>
+          <Link to="/privacy" className="hover:underline">
+            Privacy Policy
+          </Link>
+        </li>
+        <li>
+          <Link to="/terms" className="hover:underline">
+            Terms of Service
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" className="hover:underline">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </footer>
+  </div>;
+}
