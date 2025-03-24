05f9cec61baa338910dbbdce0cb6e27e6d748501
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-24 19:32:39 +0530

    add the private data members

--- Backend/src/main/java/com/raj/library/DTO/UserLibrary.java
+++ Backend/src/main/java/com/raj/library/DTO/UserLibrary.java
@@ -1,4 +1,14 @@
 package com.raj.library.DTO;
 
 public class UserLibrary {
+    private String name;
+    private float avgRating;
+    private String libraryMailId;
+    private double latitude;
+    private double longitude;
+    private String websiteLink;
+    private String openingTime;
+    private String closingTime;
+    private String openDays;
+    private String ph;
 }
