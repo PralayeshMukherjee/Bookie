8460177bd06a3e74324533ac0176633a3cf17620
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-24 15:03:39 +0530

    add a method that return list of Library find by username

--- Backend/src/main/java/com/raj/library/repository/LibraryRepo.java
+++ Backend/src/main/java/com/raj/library/repository/LibraryRepo.java
@@ -4,6 +4,9 @@ import com.raj.library.entity.Library;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LibraryRepo extends JpaRepository<Library,Long> {
+    List<Library> findByUsername(String username);
 }
